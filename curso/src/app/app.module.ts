f07f60677a9eefefafac5b94daf1c9408ea709e4
Javier <jmartin@grupoloyal.com>
2021-04-27 17:27:02 +0200

    Calculadora

--- curso/src/app/app.module.ts
+++ curso/src/app/app.module.ts
@@ -1,19 +1,29 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { environment } from 'src/environments/environment';
 import { ERROR_LEVEL, LoggerService, MyCoreModule } from 'src/my-core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CommonServicesModule } from './common-services';
 import { MainModule } from './main';
+import { CommonServicesModule } from './common-services';
+
+// Cargar idioma --------------------------------------------------------
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import localeEsExtra from '@angular/common/locales/extra/es';
+
+registerLocaleData(localeEs, 'es', localeEsExtra);
+// ---------------------------------------------------------------------
+
+import { AppComponent } from './app.component';
 import { DemosComponent } from './demos/demos.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemosComponent
+    DemosComponent,
+    CalculadoraComponent,
   ],
   imports: [
     BrowserModule, FormsModule,
@@ -23,6 +33,7 @@ import { DemosComponent } from './demos/demos.component';
   providers: [
     LoggerService,
     { provide: ERROR_LEVEL, useValue: environment.ERROR_LEVEL },
+    // { provide: LOCALE_ID, useValue: 'es-ES' },
   ],
   bootstrap: [AppComponent]
 })
