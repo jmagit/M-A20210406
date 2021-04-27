e5a2b8380ac6b479459352b5de96253c19b428a3
Javier <jmartin@grupoloyal.com>
2021-04-27 18:02:23 +0200

    solucion

--- curso/src/app/app.module.ts
+++ curso/src/app/app.module.ts
@@ -33,7 +33,7 @@ import { CalculadoraComponent } from './calculadora/calculadora.component';
   providers: [
     LoggerService,
     { provide: ERROR_LEVEL, useValue: environment.ERROR_LEVEL },
-    // { provide: LOCALE_ID, useValue: 'es-ES' },
+    { provide: LOCALE_ID, useValue: 'es-ES' },
   ],
   bootstrap: [AppComponent]
 })
